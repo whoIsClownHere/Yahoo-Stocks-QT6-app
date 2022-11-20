88b3a8b3d8fe2a2ad1410bb3df52efcb2a243dfc
whoIsClownHere <106624742+whoIsClownHere@users.noreply.github.com>
2022-11-20 11:01:56 +0300

    Update GraphWindow.py

--- Qt project/GraphWindow.py
+++ Qt project/GraphWindow.py
@@ -8,23 +8,28 @@ class GraphWindow(QWidget):
         self.setWindowTitle("Какой график построить")
         self.resize(500, 300)
 
+        # основной layout
         self.main_layout = QVBoxLayout(self)
 
         self.title = QLabel(self)
         self.title.setText("Графики")
 
+        # построить основной график
         self.portfolio_chart = QPushButton(self)
         self.portfolio_chart.setText("Построить общий график портфеля")
         self.portfolio_chart.clicked.connect(self.build_portfolio_chart)
 
+        # построить график "пирог"
         self.pie_button = QPushButton(self)
         self.pie_button.setText("Построить соотношение количества приростов и потерь портфеля")
         self.pie_button.clicked.connect(build_pie)
 
+        # построить график "ящик с усами"
         self.boxplot_button = QPushButton(self)
         self.boxplot_button.setText("Построить ящик с усами для портфеля")
         self.boxplot_button.clicked.connect(build_boxplot)
 
+        # добовление элементов в layout
         self.main_layout.addWidget(self.title)
         self.main_layout.addWidget(self.portfolio_chart)
         self.main_layout.addWidget(self.pie_button)
